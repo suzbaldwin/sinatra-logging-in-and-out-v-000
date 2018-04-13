b498d075b5efc2b397838aa47c34adc4002a21a6
suzy baldwin <gbaldwin24@comcast.net>
2018-04-13 18:47:56 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,33 +5,33 @@ class ApplicationController < Sinatra::Base
     enable :sessions unless test?
     set :session_secret, "secret"
   end
-
+  
   get '/' do
     erb :index
   end
-
+  
   post '/login' do
     @user = User.find_by(username: params["username"])
-    binding.pry
+    
     if @user.nil?
       erb :error
     else
-        session[:user_id] = @user.id
-        redirect to '/account'
+      session[:user_id] = @user.id
+      redirect to '/account'
     end
   end
-
+  
   get '/account' do
     if @user = Helpers.current_user(session)
-    erb :account
-  else
-    erb :error
+      erb :account
+    else
+      erb :error
+    end
   end
-end
   get '/logout' do
-
-
+    
+    
   end
-
-
+  
+  
 end
