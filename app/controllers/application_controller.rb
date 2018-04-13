3d8da548392dc6b37c5c6721bd277e0f96c36af1
suzy baldwin <gbaldwin24@comcast.net>
2018-04-13 18:25:31 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,18 +10,33 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
-  post '/login' do
+  # get '/login' do
+  #
+  #   erb :account
+  # end
 
+  post '/login' do
+    @user = User.find_by(username: params["username"])
+    if @user.nil?
+      erb :error
+    else
+        session[:user_id] = @user.id
+        redirect to '/account'
+    end
   end
 
   get '/account' do
-
+    @user = Helpers.current_user(session)
+    erb :account
   end
 
   get '/logout' do
+    if Helper.is_logged_in?
+      erb :account
+    else
+      erb :index
 
   end
 
 
 end
-
